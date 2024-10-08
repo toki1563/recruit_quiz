98aa13915fd184845ac3b3c3283294ce753b1045
toki1563 <sanda1563@icloud.com>
2024-10-08 18:01:35 +0900

    問題数を増やした

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// 問題文と答えをまとめる構造体
+struct Question
+{
+	string s; // 問題文
+	int a; // 答え
+};
+
 
 int main()
 {
-	cout << "[リクルート対策クイズ]\n";
-	cout << "13×(-5)の答えは？\n";
-
-	int answer;
-	cin >> answer;
-
-	if (answer == 13 * -5)
+	Question questions[] =
 	{
-		cout << "正解!正解は" << 13 * -5 << "\n";
-	}
+		{"13×(-5)" , 13 * -5},
+		{"(-21)÷(-3)", -20 / -3} ,
+		{"7-(4+2)÷2", 7 - (4 + 2) / 2},
+	};
+
+	cout << "[リクルート対策クイズ]\n";
+
+	for (const auto& e : questions)
+	{
+		cout << e.s << "の答えは？\n";
+		int answer;
+		cin >> answer;
+		if(answer == e.a)
+		{
+			cout << "正解!n";
+		}
+		else
+		{
+			cout << "間違い!!正解は" << e.a << "\n";
+		}
+	}// forquestions
 }

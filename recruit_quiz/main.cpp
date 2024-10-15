27639c61ad0444c68c89b196237ec213794bf126
toki1563 <sanda1563@icloud.com>
2024-10-15 18:54:42 +0900

    順列問題の追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -79,6 +79,18 @@ int main()
 		"サイコロを1個ふって、" + to_string(x) + "から" + to_string(x + y) + "が出る確率を求めよ。",
 		to_string((y + 1) / z) + "/" + to_string(6 / z) });
 
+	// 順列
+	x = uniform_int_distribution<>(3, 7)(rand);
+	y = uniform_int_distribution<>(1, x)(rand);
+	z = 1;
+	for (int i = 0; i < y; i++)
+	{
+		z *= x - i;
+	}
+	questions.push_back({
+		to_string(x) + "人のうち" + to_string(y) + "人を選んで並べる方法は何通りあるか？",
+		to_string(z) });
+
 
 	cout << "[リクルート対策クイズ]\n";
 

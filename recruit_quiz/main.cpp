f43d6b9ac920e77a80b631efb95d33d06f8c81bf
toki1563 <sanda1563@icloud.com>
2024-10-15 17:54:08 +0900

    円錐の体積の追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -46,6 +46,14 @@ int main()
 	questions.push_back({
 		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", x });
 
+	// 円錐の体積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 3;
+	questions.push_back({
+		"底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" +
+		"この円錐の体積をXπcm^3とする。Xの値を求めよ。",
+		x * x * y / 3 });
+
 	cout << "[リクルート対策クイズ]\n";
 
 	for (const auto& e : questions)

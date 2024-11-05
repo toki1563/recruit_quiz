79963105cfc38c68dc09abc536aba2885a97e06c
toki1563 <sanda1563@icloud.com>
2024-11-05 17:49:23 +0900

    英問題追加

--- recruit_quiz/exam_english.cpp
+++ recruit_quiz/exam_english.cpp
@@ -1,5 +1,6 @@
 #include "exam_english.h"
- #include "utility.h"
+#include "utility.h"
+#include <random>
  using namespace std;
 
  // 英語の問題
@@ -27,15 +28,64 @@
 	 QuestionList questions;
 	 questions.reserve(quizCount);
 	 const vector<int> indices = CreateRandomIndices(size(data));
+	 random_device rd;
 
-	 for (int i = 0; i < quizCount; i++) {
-		 const auto& e = data[indices[i]];
-		 questions.push_back(
-		 {"「" + string(e.meaning) + "」を意味する英単語を答えよ",
-		 e.word });
+	 // 問題の種類をランダムに選ぶ
+	 const int type = uniform_int_distribution<>(0, 3)(rd);
+	 switch (type)
+	 {
+	 case 0: // 意味から英単語を答える
+		 for (int i = 0; i < quizCount; i++) 
+		 {
+			 const auto& e = data[indices[i]];
+			 questions.push_back(
+				 { "「" + string(e.meaning) + "」を意味する英単語を答えよ",
+				 e.word });
+			 break;
+		 }
 		 break;
+	 case 1: // 「読み」から英単語を答える
+		 for (int i = 0; i < quizCount; i++) 
+		 {
+			 const auto & e = data[indices[i]];
+			 questions.push_back({
+			 "カタカナの読み「" + string(e.reading) + "」に対応する英単語を答えよ",
+			 e.word });
+			 
+		 }
+		 break;
+	 case 2: // 英単語から「読み」を答える
+		 for (int i = 0; i < quizCount; i++) 
+		 {
+			 const auto & e = data[indices[i]];
+			 questions.push_back({
+			 string(e.word) + "の読みをカタカナで答えよ",
+			 e.reading });
+			 
+		 }
+		 break;
+	 case 3: // 英単語の意味を答える
+		 for (int i = 0; i < quizCount; i++)
+		 {
+			 // 間違った番号をランダムに選ぶ
+			 const int correctIndex = indices[i];
+			 vector<int> answers = CreateWrongIndices(size(data), correctIndex);
 
-	 }
+			 // ランダムな位置を正しい番号で上書き
+			 const int correctNo = std::uniform_int_distribution<>(1, 3)(rd);
+			 answers[correctNo - 1] = correctIndex;
+
+			 // 問題文を作成
+			 string s = "「" + string(data[correctIndex].word) + "」の意味として正しい番号を選べ\n";
+			 s += std::string("  1:") + data[answers[0]].meaning + "\n";
+			 s += std::string("  2:") + data[answers[1]].meaning + "\n";
+			 s += std::string("  3:") + data[answers[2]].meaning;
+
+			 questions.push_back({ s, to_string(correctNo) });
+
+		 }
+		 break;
+	 } // switch(type)
 
 	 return questions;
  }

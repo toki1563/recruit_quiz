831e829da2ddf4329a959920dcb3285c3a05cfbc
toki1563 <sanda1563@icloud.com>
2024-11-12 17:54:07 +0900

    物理問題追加

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -17,6 +17,15 @@ QuestionList CreatePhysicsExam()
 		questions.push_back({
 			"時速" + to_string(v) + "kmで移動する車がある。\nこの車が" + to_string(v * t / 60) +
 			"km移動するのにかかる時間を分単位で求めよ", to_string(t) });
+
+		i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
+		v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i]; // 自足
+		t = uniform_int_distribution<>(1, 10)(rd) * divisors[i]; // 時間
+		questions.push_back({
+			"ある車が" + to_string(v * t / 60) + "km離れた地点まで移動するのに" + to_string(t) +
+			"分かかった。\nこの車の平均時速を求めよ", to_string(v) });
+
+
 			return questions;
 	}
 }
